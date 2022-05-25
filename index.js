330169161670e0d978d743a0874d57dc5c496602
Rawan Shorrab <89737770+RShorrab@users.noreply.github.com>
2022-05-25 12:27:36 +0200

    try deploy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config({path: './config/.env'})
+var cors = require('cors')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
@@ -7,8 +8,10 @@ const noteRouter = require('./api/note.route')
 const {dbconnection} = require('./config/dbconn')
 
 app.use(express.json())
-app.use(userRouter)
-app.use(noteRouter)
+app.use(cors())
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/notes', noteRouter)
+app.get('/', (req, res) => res.send('Hello World!'))
 app.get('*', (req, res) => res.send('Hello World!'))
 
 dbconnection()
