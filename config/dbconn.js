bbbdc31ce0e8948d2775c70bf8d7e000d5337e26
Rawan Shorrab <89737770+RShorrab@users.noreply.github.com>
2022-05-25 10:23:40 +0200

    redo it

--- config/dbconn.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-module.exports.dbconnection = ()=>
-{
-    mongoose.connect(process.env.DB_CONNECTION).then(()=> console.log('db connected')).catch((error)=> console.log(error))
-}
--- models/note.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const noteSchema = mongoose.Schema({
-    title: String, 
-    description: String,
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'user'}
-}, {timestamps: true}) 
-
-const noteModel = mongoose.model('note', noteSchema)
-module.exports = {noteModel}
